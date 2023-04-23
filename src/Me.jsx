c1d2b3fc0aa8781f94c9aad73301fb8f2af9d2d9
webamooz <sajjad.saberi1996@gmail.com>
2023-04-23 12:08:48 +0430

    Upload simple PWA project on Github

---
+++ src/Me.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import './Me.css'
+export default class Container extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is a <mark>Code</mark> Files.</h2>
+      </div>
+    )
+  }
+}
+
